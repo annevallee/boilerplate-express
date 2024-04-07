4a43639a83c81be3b0e37e0cc9dde2438d90e664
Anne Val <lakmvalencia@gmail.com>
2024-04-07 19:23:48 +0000

    ADD: use ang get METHODS

--- myApp.js
+++ myApp.js
@@ -2,10 +2,19 @@ let express = require('express');
 let app = express();
 
 console.log("Hello World");
-absolutePath = __dirname + '/views/index.html'
+absolutePath = __dirname + '/views/index.html';
+publicAbsolutePath = __dirname + "/public";
 
 app.get("/", (req, res) => {
     res.sendFile(absolutePath);
+});
+
+app.use("/public", express.static(publicAbsolutePath))
+
+app.get("/json", (req, res) => {
+    res.json({
+        "message": "Hello json"
+    });
 });
 
 
